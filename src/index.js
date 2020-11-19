642709ad6b5ef2bb5561d93a9b162ac0b3a5058c
abakarovmaks <abakarovmaks@gmail.com>
2020-11-19 12:32:28 +0200

    Создаёт файл apiService

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './main.scss';
+import './js/script.js';
+import apiServise from './js/apiService';
---
+++ src/js/apiService.js
@@ -0,0 +1,2 @@
+const BASE_URL = `https://pixabay.com/api/`;
+const KEY = '19178590-f2f628b09a5664f3e2bf6a47d';
